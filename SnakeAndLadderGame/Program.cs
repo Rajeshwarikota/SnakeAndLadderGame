95c087e228bbcbb67b5a3ac4ebe17f6dc4744d0e
Rajeshwarikota <kotarajeswari003.com>
2023-01-25 12:39:38 +0530

    [Rajeshwari] Adding Snake And Ladder Game

---
+++ SnakeAndLadderGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("....Snake And Ladder Game....");
+            Console.ReadLine();
+
+        }
+    }
+}
